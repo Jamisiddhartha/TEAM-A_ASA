4106843cf3c9d188131773add14dcb428ba6ad1d
ris3210 <rishipaul745@gmail.com>
2026-03-13 16:49:41 +0530

    Push full code

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,8 @@ const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "localhost",
   database: process.env.DB_NAME || "asadb",
-  password: process.env.DB_PASSWORD || "Jami@2003",
-  port: Number(process.env.DB_PORT) || 5432,
+  password: process.env.DB_PASSWORD || "rishi04",
+  port: Number(process.env.DB_PORT) || 5433,
 });
 
 export default pool;
